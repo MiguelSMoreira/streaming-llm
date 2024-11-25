33d189068af6cbeb0f7c60d9c59080ce6a5e2523
Jaehyun Sim <simjay@ikigailabs.io>
2024-11-25 01:26:52 -0500

    testing user input

--- examples/run_streaming_with_input.py
+++ examples/run_streaming_with_input.py
@@ -2,6 +2,7 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
+import json
 import torch
 import argparse
 from transformers import (
@@ -10,7 +11,7 @@ from transformers import (
 )
 
 from tqdm import tqdm
-# from streaming_llm.utils import load, download_url, load_jsonl
+from streaming_llm.utils import load
 from streaming_llm.enable_streaming_llm import enable_streaming_llm
 
 
@@ -60,47 +61,49 @@ def greedy_generate(model, tokenizer, input_ids, past_key_values, max_gen_len):
 def streaming_inference(model, tokenizer, prompts, kv_cache=None, max_gen_len=1000):
     past_key_values = None
     for idx, prompt in enumerate(prompts):
+        # Construct the current prompt
         prompt = "USER: " + prompt + "\n\nASSISTANT: "
-        print("\n" + "ASSISTANT: ", end="")
+        print("\n" + prompt, end="")
+
         input_ids = tokenizer(prompt, return_tensors="pt").input_ids
         input_ids = input_ids.to(model.device)
         seq_len = input_ids.shape[1]
+        # print(f"input_ids: {input_ids}")
+
         if kv_cache is not None:
             space_needed = seq_len + max_gen_len
             past_key_values = kv_cache.evict_for_space(past_key_values, space_needed)
+            # print(f"past_key_values1: {past_key_values}")
 
         past_key_values = greedy_generate(
             model, tokenizer, input_ids, past_key_values, max_gen_len=max_gen_len
         )
+        # print(f"past_key_values2: {past_key_values}")
 
 
-def load(model_name_or_path):
-    print(f"Loading model from {model_name_or_path} ...")
-    # however, tensor parallel for running falcon will occur bugs
-    import os
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_name_or_path,
-    )
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name_or_path,
-        device_map="auto",
-        torch_dtype=torch.float16,
-        local_files_only=True,
-    )
-    if tokenizer.pad_token_id is None:
-        if tokenizer.eos_token_id is not None:
-            tokenizer.pad_token_id = tokenizer.eos_token_id
-        else:
-            tokenizer.pad_token_id = 0
+def load_prompts(file_path):
+    # Read in all prompts into a list
+    with open(file_path, "r") as f:
+        file_content = json.load(f)
+    # Merge all prompts into a single string
+    return [i["content"].replace("\n", " ") for i in file_content]
 
-    model.eval()
 
-    return model, tokenizer
+def load_prompt(file_path):
+    # Read in all prompts into a list
+    with open(file_path, "r") as f:
+        file_content = json.load(f)
+    # Merge all prompts into a single string
+    return [". ".join([i["content"]for i in file_content])]
+
 
 def main():
+    # Parse the inputs
     model_name_or_path = args.model_name_or_path
     model, tokenizer = load(model_name_or_path)
+    input_prompts = load_prompt(args.input_file_path)
 
+    # Enable streaming if specified
     if args.enable_streaming:
         kv_cache = enable_streaming_llm(
             model, start_size=args.start_size, recent_size=args.recent_size
@@ -108,15 +111,20 @@ def main():
     else:
         kv_cache = None
 
-    while True:
-        raw_prompts = input("\nUSER: ")
+    # Enable RAG if specified
+    if args.enable_rag:
+        # Tokenize and ingest to the vectorDB
+        pass
+    else:
+        pass
 
-        streaming_inference(
-            model,
-            tokenizer,
-            [raw_prompts],
-            kv_cache,
-        )
+    # Run streamingLLM inference
+    streaming_inference(
+        model=model,
+        tokenizer=tokenizer,
+        prompts=input_prompts,
+        kv_cache=kv_cache,
+    )
 
 
 if __name__ == "__main__":
@@ -126,6 +134,8 @@ if __name__ == "__main__":
     )
     parser.add_argument("--data_root", type=str, default="data/")
     parser.add_argument("--enable_streaming", action="store_true")
+    parser.add_argument("--enable_rag", action="store_true")
+    parser.add_argument("--input_file_path", type=str)
     parser.add_argument("--start_size", type=int, default=4)
     parser.add_argument("--recent_size", type=int, default=2000)
     args = parser.parse_args()
